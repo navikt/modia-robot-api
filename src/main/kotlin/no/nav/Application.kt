6450f139e3ebaff35c487c582257f64ed44de412
Nicklas Utgaard <nutgaard@gmail.com>
2022-06-23 15:16:44 +0200

    [OPP-1487] starter appen uten sikkerhet for nå

--- src/main/kotlin/no/nav/Application.kt
+++ src/main/kotlin/no/nav/Application.kt
@@ -20,7 +20,7 @@ fun startApplication(
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         environment.config
         configureOpenApi()
-        configureSecurity(disableSecurity)
+        configureSecurity(disableSecurity, "")
         configureMonitoring()
         configureSerialization()
 
@@ -40,5 +40,5 @@ fun startApplication(
 }
 
 fun main() {
-    startApplication(disableSecurity = false)
+    startApplication(disableSecurity = true)
 }
