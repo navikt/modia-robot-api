a2f21d7b18b8862501afd516387596dadc3eefa7
Nicklas Utgaard <nutgaard@gmail.com>
2022-07-28 14:39:20 +0200

    [OPP-1487] mockenv slik at appen kan startes lokalt

--- src/test/kotlin/no/nav/RunLocally.kt
+++ src/test/kotlin/no/nav/RunLocally.kt
@@ -1,12 +1,13 @@
 package no.nav
 
 import no.nav.mock.MockConsumers
+import no.nav.mock.MockEnv
 
 fun main() {
     System.setProperty("IDENT_ALLOW_LIST", "")
     startApplication(
         disableSecurity = true,
-        env = EnvImpl(),
+        env = MockEnv,
         consumers = MockConsumers,
     )
 }
---
+++ src/test/kotlin/no/nav/mock/MockEnv.kt
@@ -0,0 +1,12 @@
+package no.nav.mock
+
+import no.nav.Env
+
+object MockEnv : Env {
+    override val soapStsUrl: String = ""
+    override val jwksUrl: String = ""
+    override val oppfolgingUrl: String = ""
+    override val tpsPersonV3Url: String = ""
+    override val nomUrl: String = ""
+    override val identAllowList: List<String> = emptyList()
+}
