9e94755eed9253842399c6e70903677ef2340525
Nicklas Utgaard <nutgaard@gmail.com>
2022-08-04 09:03:00 +0200

    [KAIZEN-0] fikse deserialisering av tekster
    trenger en egen MapSerializer slik at ktor greier å forstå hvordan `Tekster` aliaset skal deseraliseres

--- src/main/kotlin/no/nav/utils/SerializationUtils.kt
+++ src/main/kotlin/no/nav/utils/SerializationUtils.kt
@@ -12,11 +12,11 @@ import kotlin.reflect.KClass
 
 object UUIDSerializer : KSerializer<UUID> {
     override val descriptor = PrimitiveSerialDescriptor("UUID", PrimitiveKind.STRING)
-    
+
     override fun deserialize(decoder: Decoder): UUID {
         return UUID.fromString(decoder.decodeString())
     }
-    
+
     override fun serialize(encoder: Encoder, value: UUID) {
         encoder.encodeString(value.toString())
     }
