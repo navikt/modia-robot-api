7f48948612795cbb2b4b9e3312666f31c4ac4f35
vildestefferud <qwartz.ms@hotmail.com>
2022-08-10 16:48:46 +0200

    [OPP-1491] Legger til temagruppe-mapping

---
+++ src/main/kotlin/no/nav/api/dialog/sf/Temagruppe.kt
@@ -0,0 +1,10 @@
+package no.nav.api.dialog.sf
+
+enum class Temagruppe(val kode: String) {
+    ARBD("ARBD_KNA"),
+    FMLI("FMLI_KNA"),
+    HJLPM("HJLPM_KNA"),
+    OVRG("OVRG_KNA"),
+    PENS("PENS_KNA")
+}
+
