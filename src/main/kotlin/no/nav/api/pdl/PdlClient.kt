21d888d83856916986f275f11112656fa112bce2
vildestefferud <qwartz.ms@hotmail.com>
2022-08-19 10:26:30 +0200

    [KAIZEN-0] Bytter til å bruke GraphQlKtorClient fra bibliotek i stedet for custome og oppdaterer tester
    Slipper nå å håndtere serialisering av Any selv da dette biblioteket blant annet tilbyr AnySerializer

--- src/main/kotlin/no/nav/api/pdl/PdlClient.kt
+++ src/main/kotlin/no/nav/api/pdl/PdlClient.kt
@@ -1,45 +1,56 @@
 package no.nav.api.pdl
 
-import HentAktorid
+import com.expediagroup.graphql.client.types.GraphQLClientResponse
+import io.ktor.client.*
 import io.ktor.client.engine.*
 import io.ktor.client.engine.okhttp.*
 import io.ktor.client.request.*
-import no.nav.api.pdl.queries.HentPersonalia
+import no.nav.api.generated.pdl.HentPersonalia
+import no.nav.api.generated.pdl.HentAktorid
+import no.nav.api.generated.pdl.enums.IdentGruppe
 import no.nav.utils.*
+import java.net.URL
 
 class PdlClient(
     private val pdlUrl: String,
     private val tokenclient: BoundedMachineToMachineTokenClient,
     httpEngine: HttpClientEngine = OkHttp.create()
 ) {
-    private val graphqlClient = GraphQLClient(
-        httpClient = GraphQLClient.createHttpClient(httpEngine),
-        config = GraphQLClientConfig(
-            serviceName = "PDL",
-            critical = false,
-            requestConfig = {
-                val token = tokenclient.createMachineToMachineToken()
-                url(pdlUrl)
-                header("Nav-Consumer-Token", "Bearer $token")
-                header("Authorization", "Bearer $token")
-                header("Tema", "GEN")
-                header("X-Correlation-ID", getCallId())
-            }
-        )
+    private val graphqlKtorClient = LoggingGraphQLKtorClient2(
+        name = "PDL",
+        critical = false,
+        url = URL(pdlUrl),
+        httpClient = HttpClient(httpEngine)
     )
 
-    suspend fun hentPersonalia(fnr: String): GraphQLResponse<HentPersonalia.Result> {
+    private val requestConfig: HeadersBuilder = {
+        val token = tokenclient.createMachineToMachineToken()
+        url(pdlUrl)
+        header("Nav-Consumer-Token", "Bearer $token")
+        header("Authorization", "Bearer $token")
+        header("Tema", "GEN")
+        header("X-Correlation-ID", getCallId())
+    }
+
+    suspend fun hentPersonalia(fnr: String): GraphQLClientResponse<HentPersonalia.Result> {
         return externalServiceCall {
-            graphqlClient.execute(
-                HentPersonalia(HentPersonalia.Variables(fnr))
+            graphqlKtorClient.execute(
+                request = HentPersonalia(HentPersonalia.Variables(fnr)),
+                requestCustomizer = requestConfig
             )
         }
     }
 
-    suspend fun hentAktorid(fnr: String): GraphQLResponse<HentAktorid.Result> {
+    suspend fun hentAktorid(fnr: String): GraphQLClientResponse<HentAktorid.Result> {
         return externalServiceCall {
-            graphqlClient.execute(
-                HentAktorid(HentAktorid.Variables(fnr))
+            graphqlKtorClient.execute(
+                request = HentAktorid(
+                    HentAktorid.Variables(
+                        ident = fnr,
+                        grupper = listOf(IdentGruppe.AKTORID)
+                    )
+                ),
+                requestCustomizer = requestConfig
             )
         }
     }
