3a29f7e1ca703cc8751d06f5fa91839d275fd38f
Eirik Dahlen <dahleneirik@gmail.com>
2022-09-06 13:36:10 +0200

    [OPP-1499] håndtere feil ved tilgangskontroll
    Det er ønskelig å logge litt mer dersom man får 500 eller 401 ved autentisering av token.

--- src/main/kotlin/no/nav/plugins/ExceptionHandling.kt
+++ src/main/kotlin/no/nav/plugins/ExceptionHandling.kt
@@ -1,6 +1,7 @@
 package no.nav.plugins
 
 import io.ktor.application.*
+import io.ktor.auth.*
 import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.response.*
@@ -11,18 +12,38 @@ class WebStatusException(message: String, val status: HttpStatusCode) : Exceptio
 fun Application.configureExceptionHandling() {
     install(StatusPages) {
         exception<WebStatusException> { cause ->
+            log.warn("WebStatusException:${cause.status.value}", cause)
             call.respond(
                 cause.status,
                 HttpErrorResponse(
                     cause = cause.toString(),
                     message = cause.message,
-                    
                 )
             )
         }
-        exception<Throwable> {
-            call.respond(HttpStatusCode.InternalServerError)
+        exception<Throwable> { cause ->
+            log.error("Unhandled exception", cause)
+            call.respond(
+                HttpStatusCode.InternalServerError,
+                HttpErrorResponse(
+                    cause = cause.toString(),
+                    message = cause.message,
+                )
+            )
         }
+        status(HttpStatusCode.Unauthorized) { statusCode ->
+            val message = call.authentication.allFailures.joinToString("\n") { it.prettyPrint() }
+            log.error(message)
+            call.respond(statusCode, message)
+        }
+    }
+}
+
+private fun AuthenticationFailedCause.prettyPrint(): String {
+    return when (this) {
+        AuthenticationFailedCause.NoCredentials -> "No credentials"
+        AuthenticationFailedCause.InvalidCredentials -> "Invalid credentials"
+        is AuthenticationFailedCause.Error -> "Error with credentials: ${this.message}"
     }
 }
 
