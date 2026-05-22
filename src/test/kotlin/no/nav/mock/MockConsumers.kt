1f51595b683858b60f012f1b0e7d260b9c8af7a0
martinsomm-nav <martin.dagfinn.boler.sommerseth@nav.no>
2026-05-22 14:08:52 +0200

    Lint fix

--- src/test/kotlin/no/nav/mock/MockConsumers.kt
+++ src/test/kotlin/no/nav/mock/MockConsumers.kt
@@ -240,7 +240,7 @@ private val pdlClientMock =
                                     ),
                                 kontaktadresse = emptyList(),
                                 bostedsadresse = emptyList(),
-                                statsborgerskap = emptyList()
+                                statsborgerskap = emptyList(),
                             ),
                     ),
             )
