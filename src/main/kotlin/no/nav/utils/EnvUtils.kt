525d5f0c08e8e7e9f39f60833801c1c3f5ac2949
Nummedal <sigrun.asheim.nummedal@nav.no>
2026-09-11 10:33:06 +0200

    Genrer versjonsnummer på open API spev basert på dockerbuild - timestamp + git sha

--- src/main/kotlin/no/nav/utils/EnvUtils.kt
+++ src/main/kotlin/no/nav/utils/EnvUtils.kt
@@ -4,6 +4,18 @@ import no.nav.personoversikt.common.utils.EnvUtils
 
 val appImage: String = EnvUtils.getConfig("NAIS_APP_IMAGE") ?: "N/A"
 
+/**
+ * Versjonen av applikasjonen, hentet fra taggen i `NAIS_APP_IMAGE`.
+ *
+ * Nais setter imaget til `ghcr.io/navikt/modia-robot-api:<tag>`. Taggen settes av byggeprosessen og
+ * er det nærmeste vi kommer et versjonsnummer. Lokalt og i tester finnes ikke variabelen, og da
+ * brukes `"lokal"`.
+ */
+val appVersjon: String =
+    appImage
+        .substringAfterLast(':', "")
+        .ifBlank { "lokal" }
+
 private val prodClusters = arrayOf("prod-fss", "prod-sbs", "prod-gcp")
 
 fun isProd(): Boolean = prodClusters.contains(EnvUtils.getConfig("NAIS_CLUSTER_NAME"))
